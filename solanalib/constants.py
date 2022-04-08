d860e791879bf983e1eda8ef3b3573d4fa11bd56
Fabian Stermann <fabian.stermann@gmail.com>
2022-04-08 20:51:45 +0200

    fix enum type

--- solanalib/constants.py
+++ solanalib/constants.py
@@ -1,18 +1,18 @@
 from enum import Enum
 
 
-class MagicEden(Enum):
+class MagicEden(str, Enum):
     PROGRAM_V1 = "MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8"
     PROGRAM_V2 = "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
     AUTHORITY = "GUfCR9mK6azb9vcpsxgXyj7XRPAKJd4KMHTTVvtncGgp"
     CANCEL_LISTING_INSTRUCTION = "TE6axTojnpk"
 
 
-class Metaplex(Enum):
+class Metaplex(str, Enum):
     METADATA_PROGRAM = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
     CANDY_MACHINE_V2 = "cndy3Z4yapfJBmL3ShUp5exZKqR3z33thTzeNMm2gRZ"
 
 
-class Token(Enum):
+class Token(str, Enum):
     PROGRAM = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
     ASSOCIATE_PROGRAM = "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
