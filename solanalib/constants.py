1ed2f5b64d6adaa0b1cb837752d3f5d4d9b1497b
Fabian Stermann <fabian.stermann@gmail.com>
2022-04-09 15:12:42 +0200

    add marketplaces

--- solanalib/constants.py
+++ solanalib/constants.py
@@ -1,10 +1,16 @@
 from enum import Enum
 
 
+class Marketplace(str, Enum):
+    MAGIC_EDEN_V1 = "MagicEdenV1"
+    MAGIC_EDEN_V2 = "MagicEdenV2"
+
+
 class MagicEden(str, Enum):
     PROGRAM_V1 = "MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8"
     PROGRAM_V2 = "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
-    AUTHORITY = "GUfCR9mK6azb9vcpsxgXyj7XRPAKJd4KMHTTVvtncGgp"
+    AUTHORITY_V1 = "GUfCR9mK6azb9vcpsxgXyj7XRPAKJd4KMHTTVvtncGgp"
+    AUTHORITY_V2 = "1BWutmTvYPwDtmw9abTkS4Ssr8no61spGAvW1X6NDix"
     CANCEL_LISTING_INSTRUCTION = "TE6axTojnpk"
 
 
