16af3019fe11243a21434e14f9d83b2da2eb947e
Fabian Stermann <fabian.stermann@gmail.com>
2022-04-16 12:53:26 +0200

    add mint activity method

--- solanalib/nft/methods.py
+++ solanalib/nft/methods.py
@@ -1,10 +1,11 @@
 from typing import List
 
 from solanalib.logger import logger
-from solanalib.nft.models import Activity, NFTTransaction
+from solanalib.nft.models import Activity, ActivityType, NFTTransaction
 from solanalib.nft.parse import parse_transaction
 from solanalib.publickey import PublicKey
 from solanalib.rpc.client import Client
+from solanalib.rpc.proclient import ProClient
 
 
 def get_collection_activities(creator_address):
@@ -18,6 +19,44 @@ def get_collection_activities(creator_address):
     #       if last tx is transfer
     #           repeat from *
     pass
+
+
+def get_mint_activities(
+    token_mint: PublicKey, client: ProClient = ProClient()
+) -> List[Activity]:
+    current_token_account = get_current_token_account(
+        token_mint=token_mint, client=client
+    )
+    all_activities = []
+
+    while current_token_account:
+        logger.debug(
+            f"Fetching all activites for token account {current_token_account}"
+        )
+        activities = get_mint_activites_for_token_account(
+            token_mint=token_mint, token_account=current_token_account, client=client
+        )
+        all_activities += activities
+
+        last_activity = activities[-1]
+        if last_activity.type_ == ActivityType.TRANSFER:
+            current_token_account = last_activity.new_token_account
+        else:
+            logger.debug(
+                f"Found all activites for token account {current_token_account}"
+            )
+            current_token_account is None
+
+    return all_activities
+
+
+def get_mint_activites_for_token_account(
+    token_mint: str, token_account: str, client: ProClient = ProClient()
+) -> List[Activity]:
+    transactions = client.get_all_parsed_transactions_for_address(account=token_account)
+    txs = [NFTTransaction(tx, mint=token_mint) for tx in transactions]
+    activities = parse_all_transactions(transactions=txs)
+    return activities
 
 
 def get_current_token_account(tokent_mint: PublicKey, client: Client = Client()):
