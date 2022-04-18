1884294adaf1d511f8f9af3180869b10ec76aa23
Fabian Stermann <fabian.stermann@gmail.com>
2022-04-18 18:55:30 +0200

    add google endpoint

--- solanalib/rpc/endpoints.py
+++ solanalib/rpc/endpoints.py
@@ -4,6 +4,7 @@ from enum import Enum
 class Endpoint(str, Enum):
     DEFAULT = "https://rpc.theindex.io"
     MAINNET_BETA = "https://api.mainnet-beta.solana.com"
+    MAINNET_GOOGLE = "https://api.google.mainnet-beta.solana.com/"
     GENESYS_GO = "https://ssc-dao.genesysgo.net"
     PROJECT_SERUM = "https://solana-api.projectserum.com"
     THE_INDEX = "https://rpc.theindex.io"
