507d66aa7ecfbb799be3abbc73d519159121374f
Fabian Stermann <fabian.stermann@gmail.com>
2022-04-18 20:24:39 +0200

    add tests for sales mev2 parser

--- tests/nft/parse/test_parse_sales.py
+++ tests/nft/parse/test_parse_sales.py
@@ -15,12 +15,29 @@ class TestParseSale:
         assert activity.marketplace == MagicEdenV1.MARKETPLACE
 
     def test_parse_sale_mev2(self, load_tx):
-        pass
-        # tx = load_tx("sales", "mev2_01")
-        # mint = "Bvn2AsrHX2g2SVH3ByRRhK2qbCDeH1jUmJSWVPzrob5Q"
-        # activity = parse_sale(tx=tx, mint=mint)
-        # assert isinstance(activity, SaleActivity)
-        # assert activity.mint == mint
-        # assert activity.new_authority == "BPwzfbHv3JW98kZ3HPJTardvD9pbuD9dVGtoLxiysfk6"
-        # assert activity.price_lamports == 3000000000
-        # assert activity.marketplace == MagicEdenV2.MARKETPLACE
+        tx = load_tx("sales", "mev2_01")
+        mint = "Bvn2AsrHX2g2SVH3ByRRhK2qbCDeH1jUmJSWVPzrob5Q"
+        activity = parse_sale(tx=tx, mint=mint)
+        assert isinstance(activity, SaleActivity)
+        assert activity.mint == mint
+        assert activity.new_authority == "BPwzfbHv3JW98kZ3HPJTardvD9pbuD9dVGtoLxiysfk6"
+        assert activity.price_lamports == 3000000000
+        assert activity.marketplace == MagicEdenV2.MARKETPLACE
+
+        tx = load_tx("sales", "mev2_02")
+        mint = "GMXXVkCnikqj2ngbv12yrypBhN6idL8EV335k55oqXDP"
+        activity = parse_sale(tx=tx, mint=mint)
+        assert isinstance(activity, SaleActivity)
+        assert activity.mint == mint
+        assert activity.new_authority == "GDNV3QKvLewWBQif1jpNHCJFYgV3F6GT7ZDD1ruoTkWC"
+        assert activity.price_lamports == 17000000000
+        assert activity.marketplace == MagicEdenV2.MARKETPLACE
+
+        tx = load_tx("sales", "mev2_03")
+        mint = "DmwPBYvS8D5GSqYtuT2nV51xipqxLo938uYb4NCQjWB7"
+        activity = parse_sale(tx=tx, mint=mint)
+        assert isinstance(activity, SaleActivity)
+        assert activity.mint == mint
+        assert activity.new_authority == "ENdJXpv18wNPiSu7fCkPGHvqM7GRxLdWBBVgzZMbKd7R"
+        assert activity.price_lamports == 3500000000
+        assert activity.marketplace == MagicEdenV2.MARKETPLACE
