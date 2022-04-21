00525413425631cacf3dc0b0cb5b3beb732d8210
Fabian Stermann <fabian.stermann@gmail.com>
2022-04-21 16:44:22 +0200

    change default endpoint

--- solanalib/rpc/endpoints.py
+++ solanalib/rpc/endpoints.py
@@ -2,9 +2,9 @@ from enum import Enum
 
 
 class Endpoint(str, Enum):
-    DEFAULT = "https://rpc.theindex.io"
+    DEFAULT = "https://api.google.mainnet-beta.solana.com"
     MAINNET_BETA = "https://api.mainnet-beta.solana.com"
-    MAINNET_GOOGLE = "https://api.google.mainnet-beta.solana.com/"
+    MAINNET_GOOGLE = "https://api.google.mainnet-beta.solana.com"
     GENESYS_GO = "https://ssc-dao.genesysgo.net"
     PROJECT_SERUM = "https://solana-api.projectserum.com"
     THE_INDEX = "https://rpc.theindex.io"
