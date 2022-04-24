0e35232938d8bd2e3e03b945eb4d0bd9393c4b0f
Fabian Stermann <fabian.stermann@gmail.com>
2022-04-24 17:52:33 +0200

    add base instruction class

--- solanalib/nft/models.py
+++ solanalib/nft/models.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from typing import Dict, List
+from lib2to3.pgen2 import token
+from typing import Dict, List, Union
 
 from pydantic import BaseModel
 from solanalib.constants import Marketplace
@@ -7,11 +8,7 @@ from solanalib.util import SafeDict
 from solanalib.logger import logger
 
 
-class OuterInstruction(SafeDict):
-    @property
-    def data(self) -> SafeDict:
-        return self["data"]
-
+class Instruction(SafeDict):
     @property
     def parsed(self) -> SafeDict:
         return self["parsed"]
@@ -20,8 +17,17 @@ class OuterInstruction(SafeDict):
     def info(self) -> SafeDict:
         return self.parsed["info"]
 
-    def has_data(self) -> bool:
-        return "data" in self
+    def get_authority(self) -> Union[str, None]:
+        if "authority" in self.info:
+            return self.info["authority"]
+        if "multisigAuthority" in self.info:
+            return self.info["multisigAuthority"]
+
+        logger.debug("Authority not found in parsed tx")
+        return None
+
+    def is_type(self, ttype: str) -> bool:
+        return self.parsed["type"] == ttype
 
     def is_program(self, program: str) -> bool:
         return self["program"] == program
@@ -29,15 +35,29 @@ class OuterInstruction(SafeDict):
     def is_program_id(self, program_id: str) -> bool:
         return self["programId"] == program_id
 
-    def is_type(self, _type: str) -> bool:
-        return self.parsed["type"] == _type
+    def is_spl_token_transfer(self) -> bool:
+        if not (self.is_program("spl-token") and self.parsed["type"] == "transfer"):
+            return False
+        logger.debug(f"Is program spl-token and type {self.parsed['type']}")
 
-    def is_mint(self, mint: str) -> bool:
-        return (
-            self.is_type("mintTo")
-            and self.is_program("spl-token")
-            and self.info["mint"] == mint
-        )
+        if not self.info["amount"] == "1":
+            return False
+        logger.debug("Transferred amount 1")
+
+        return True
+
+    def is_spl_token_checked_transfer(self) -> bool:
+        if not (
+            self.is_program("spl-token") and self.parsed["type"] == "transferChecked"
+        ):
+            return False
+        logger.debug(f"Is program spl-token and type {self.parsed['type']}")
+
+        if not self.info["tokenAmount"]["amount"] == "1":
+            return False
+        logger.debug("Transferred amount 1")
+
+        return True
 
     def is_create_associate_account_for_mint(self, mint: str) -> bool:
         if not (
@@ -63,34 +83,28 @@ class OuterInstruction(SafeDict):
 
         return True
 
-    def is_spl_token_transfer(self) -> bool:
-        if not (
-            self.is_program("spl-token")
-            and self.parsed["type"]
-            in [
-                "transferChecked",
-                "transfer",
-            ]
-        ):
-            return False
-        logger.debug(f"Is program spl-token and type {self.parsed['type']}")
 
-        if not self.info["amount"] == "1":
-            return False
-        logger.debug("Transferred amount 1")
-
-        return True
-
-
-class InnerInstruction(SafeDict):
+class OuterInstruction(Instruction):
     @property
-    def parsed(self) -> SafeDict:
-        return self["parsed"]
+    def is_parsed(self) -> bool:
+        return "parsed" in self
 
     @property
-    def info(self) -> SafeDict:
-        return self.parsed["info"]
+    def data(self) -> SafeDict:
+        return self["data"]
 
+    def has_data(self) -> bool:
+        return "data" in self
+
+    def is_mint(self, mint: str) -> bool:
+        return (
+            self.is_type("mintTo")
+            and self.is_program("spl-token")
+            and self.info["mint"] == mint
+        )
+
+
+class InnerInstruction(Instruction):
     @property
     def authority(self) -> SafeDict:
         return self.info["authority"]
@@ -102,12 +116,6 @@ class InnerInstruction(SafeDict):
     @property
     def source(self) -> SafeDict:
         return self.info["source"]
-
-    def is_type(self, type_: str) -> bool:
-        return self.parsed["type"] == type_
-
-    def is_program(self, program: str) -> bool:
-        return self["program"] == program
 
     def is_create_account_by_program(self, program: str) -> bool:
         return (
@@ -152,6 +160,9 @@ class Transaction(BaseModel):
     block_time: int
     slot: int
     instructions: Instructions
+    pre_token_balances: List[SafeDict]
+    post_token_balances: List[SafeDict]
+    accounts: List[SafeDict]
 
     def __init__(self, transaction: dict, *args, **kwargs):
         super().__init__(
@@ -159,9 +170,35 @@ class Transaction(BaseModel):
             block_time=transaction["blockTime"],
             slot=transaction["slot"],
             instructions=Instructions(transaction),
+            pre_token_balances=[
+                SafeDict(bal) for bal in transaction["meta"]["preTokenBalances"]
+            ],
+            post_token_balances=[
+                SafeDict(bal) for bal in transaction["meta"]["postTokenBalances"]
+            ],
+            accounts=[
+                SafeDict(acc)
+                for acc in transaction["transaction"]["message"]["accountKeys"]
+            ],
             *args,
             **kwargs,
         )
+
+    def get_pre_owner(self, mint: str):
+        for token_balance in self.pre_token_balances:
+            if (
+                token_balance["mint"] == mint
+                and token_balance["uiTokenAmount"]["amount"] == "1"
+            ):
+                return token_balance["owner"]
+
+    def get_post_owner(self, mint: str):
+        for token_balance in self.post_token_balances:
+            if (
+                token_balance["mint"] == mint
+                and token_balance["uiTokenAmount"]["amount"] == "1"
+            ):
+                return token_balance["owner"]
 
 
 class NFTTransaction(Transaction):
