d77e5a3474e693e8ad96c6a030300f10b2c811a8
Fabian Stermann <fabian.stermann@gmail.com>
2022-04-25 16:34:30 +0200

    parse generic mint

--- solanalib/nft/parsers/mints.py
+++ solanalib/nft/parsers/mints.py
@@ -5,6 +5,26 @@ from solanalib.logger import logger
 from solanalib.nft.models import MintActivity, Transaction
 
 
+def parse_mint_other(tx: Transaction, mint: str) -> Union[MintActivity, None]:
+    for ix in tx.instructions.outer:
+        if ix.is_mint(mint):
+            logger.debug("Is correct mint tx")
+            new_authority = ix.info["mintAuthority"]
+            new_token_account = ix.info["account"]
+            program = ix["program"]
+
+            return MintActivity(
+                transaction_id=tx.transaction_id,
+                block_time=tx.block_time,
+                slot=tx.slot,
+                mint=mint,
+                new_authority=new_authority,
+                new_token_account=new_token_account,
+                program=program,
+            )
+    return None
+
+
 def parse_mint_candy_machine_v1(
     tx: Transaction, mint: str
 ) -> Union[MintActivity, None]:
@@ -69,6 +89,7 @@ def parse_mint(tx: Transaction, mint: str) -> Union[MintActivity, None]:
     to_parse = {
         "CandyMachineV1": parse_mint_candy_machine_v1,
         "CandyMachineV2": parse_mint_candy_machine_v2,
+        "OtherMint": parse_mint_other,
     }
 
     for marketplace, parser in to_parse.items():
