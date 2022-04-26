a8d4749f27ed4c6a39c703a1c93f592414bbc1a8
Fabian Stermann <fabian.stermann@gmail.com>
2022-04-26 09:51:28 +0200

    prettify logger

--- solanalib/logger.py
+++ solanalib/logger.py
@@ -1,5 +1,14 @@
 import logging
 
 logger = logging.getLogger()
-logger.addHandler(logging.StreamHandler())
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    "[%(asctime)s:%(msecs)d] %(levelname)-8s %(message)s", "%Y-%m-%d %H:%M:%S"
+)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
 logger.setLevel(logging.DEBUG)
