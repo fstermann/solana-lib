c6451ebc4579ab8f05079be71f77528f6f677e1e
Fabian Stermann <fabian.stermann@gmail.com>
2022-04-28 12:45:01 +0200

    refactor delisting method

--- solanalib/nft/parsers/delistings.py
+++ solanalib/nft/parsers/delistings.py
@@ -1,65 +1,87 @@
 from typing import Union
 
-from solanalib.constants import MagicEdenV1, MagicEdenV2
 from solanalib.logger import logger
 from solanalib.nft.activities import DelistingActivity
+from solanalib.nft.instructions import Instruction
+from solanalib.nft.marketplaces import marketplaces
 from solanalib.nft.transaction import Transaction
 
 
-def parse_delisting_mev1(tx: Transaction, mint: str) -> Union[DelistingActivity, None]:
+def parse_delisting_mev1(tx: Transaction) -> Union[DelistingActivity, None]:
+    def parse_ix(ix: Instruction) -> Union[DelistingActivity, None]:
+        marketplace = marketplaces.magic_eden_v1
+
+        if not ix.is_program_id(marketplace.program):
+            return None
+        logger.debug(f"Program is {marketplace.name}")
+
+        if not ix.data[0:10] == marketplace.instructions.delisting.base64_prefix:
+            return None
+        logger.debug("Is Delisting instruction")
+
+        account_indices = marketplace.instructions.delisting.account_indices
+
+        old_authority = marketplace.authority
+        new_authority = ix["accounts"][account_indices.new_authority]
+        old_token_account = ix["accounts"][account_indices.old_token_account]
+
+        mint = tx.get_mint_by_accounts(old_authority, new_authority, old_token_account)
+
+        return DelistingActivity(
+            transaction_id=tx.transaction_id,
+            block_time=tx.block_time,
+            slot=tx.slot,
+            mint=mint,
+            old_authority=old_authority,
+            new_authority=new_authority,
+            old_token_account=old_token_account,
+            new_token_account=old_token_account,
+            program=marketplace.name,
+        )
+
     for ix in tx.instructions.outer:
-        if not ix.is_program_id(MagicEdenV1.PROGRAM):
-            continue
-        logger.debug(f"Program is {MagicEdenV1.NAME}")
-        marketplace = MagicEdenV1.MARKETPLACE
-
-        if ix.data[0:10] == MagicEdenV1.DELISTING_INSTRUCTION:
-            logger.debug("Is Delisting instruction")
-            new_authority = ix["accounts"][0]  # 1st account
-            old_authority = MagicEdenV1.AUTHORITY
-            old_token_account = ix["accounts"][1]
-
-            return DelistingActivity(
-                transaction_id=tx.transaction_id,
-                block_time=tx.block_time,
-                slot=tx.slot,
-                mint=mint,
-                old_authority=old_authority,
-                new_authority=new_authority,
-                old_token_account=old_token_account,
-                new_token_account=old_token_account,
-                program=marketplace,
-            )
+        activity = parse_ix(ix=ix)
+        if activity:
+            return activity
     return None
 
 
-def parse_delisting_mev2(tx: Transaction, mint: str) -> Union[DelistingActivity, None]:
+def parse_delisting_mev2(tx: Transaction) -> Union[DelistingActivity, None]:
+    def parse_ix(ix: Instruction) -> Union[DelistingActivity, None]:
+        marketplace = marketplaces.magic_eden_v2
+
+        if not ix.is_program_id(marketplace.program):
+            return None
+        logger.debug(f"Program is {marketplace.name}")
+
+        if not ix.data[0:10] == marketplace.instructions.delisting.base64_prefix:
+            return None
+        logger.debug("Is Delisting instruction")
+
+        account_indices = marketplace.instructions.delisting.account_indices
+
+        old_authority = marketplace.authority
+        new_authority = ix["accounts"][account_indices.new_authority]
+        old_token_account = ix["accounts"][account_indices.old_token_account]
+
+        mint = ix["accounts"][account_indices.mint]
+
+        return DelistingActivity(
+            transaction_id=tx.transaction_id,
+            block_time=tx.block_time,
+            slot=tx.slot,
+            mint=mint,
+            old_authority=old_authority,
+            new_authority=new_authority,
+            old_token_account=old_token_account,
+            new_token_account=old_token_account,
+            program=marketplace.name,
+        )
+
     for ix in tx.instructions.outer:
-        if not ix.is_program_id(MagicEdenV2.PROGRAM):
-            continue
-        logger.debug(f"Program is {MagicEdenV2.NAME}")
-        marketplace = MagicEdenV2.MARKETPLACE
-
-        if ix.data[0:10] == MagicEdenV2.DELISTING_INSTRUCTION:
-            logger.debug("Is Delisting Instruction")
-            if ix["accounts"][3] != mint:  # 4th account
-                logger.debug("Mint did not match")
-
-            new_authority = ix["accounts"][0]  # 1st account
-            old_authority = MagicEdenV2.AUTHORITY
-            old_token_account = ix["accounts"][2]
-
-            return DelistingActivity(
-                transaction_id=tx.transaction_id,
-                block_time=tx.block_time,
-                slot=tx.slot,
-                mint=mint,
-                old_authority=old_authority,
-                new_authority=new_authority,
-                old_token_account=old_token_account,
-                new_token_account=old_token_account,
-                program=marketplace,
-            )
+        activity = parse_ix(ix=ix)
+        if activity:
+            return activity
     return None
 
 
@@ -71,7 +93,7 @@ def parse_delisting(tx: Transaction, mint: str):
 
     for marketplace, parser in to_parse.items():
         logger.info(f"Checking marketplace {marketplace}")
-        activity = parser(tx=tx, mint=mint)
+        activity = parser(tx=tx)
         if activity:
             return activity
 
