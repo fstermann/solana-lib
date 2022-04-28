e75432a86adffd894af8195e1e819109d1093f70
Fabian Stermann <fabian.stermann@gmail.com>
2022-04-28 13:37:03 +0200

    refactor listings parser

--- solanalib/nft/parsers/listings.py
+++ solanalib/nft/parsers/listings.py
@@ -1,76 +1,95 @@
 from typing import Union
 
-from solanalib.constants import AuctionHouse, MagicEdenV1, MagicEdenV2
+from solanalib.constants import AuctionHouse
 from solanalib.logger import logger
 from solanalib.nft.activities import ListingActivity
+from solanalib.nft.instructions import Instruction
+from solanalib.nft.marketplaces import marketplaces
 from solanalib.nft.transaction import Transaction
 
 from .util import get_me_lamports_price_from_data
 
 
 def parse_listing_mev1(tx: Transaction, mint: str) -> Union[ListingActivity, None]:
+    def parse_ix(ix: Instruction) -> Union[ListingActivity, None]:
+        marketplace = marketplaces.magic_eden_v1
+
+        if not ix.is_program_id(marketplace.program):
+            return None
+        logger.debug(f"Program is {marketplace.name}")
+
+        if not ix.data[0:10] == marketplace.instructions.listing.base64_prefix:
+            return None
+        logger.debug("Is Listing instruction")
+
+        account_indices = marketplace.instructions.listing.account_indices
+
+        old_authority = ix["accounts"][account_indices.old_authority]
+        new_authority = marketplace.authority
+        old_token_account = ix["accounts"][account_indices.old_token_account]
+
+        price_lamports = get_me_lamports_price_from_data(ix.data, marketplace.program)
+
+        mint = tx.get_mint_by_accounts(old_authority, new_authority, old_token_account)
+
+        return ListingActivity(
+            transaction_id=tx.transaction_id,
+            block_time=tx.block_time,
+            slot=tx.slot,
+            mint=mint,
+            old_authority=old_authority,
+            new_authority=new_authority,
+            old_token_account=old_token_account,
+            new_token_account=old_token_account,  # In MEv1 token account doesn't change
+            price_lamports=price_lamports,
+            program=marketplace.name,
+        )
+
     for ix in tx.instructions.outer:
-        if not ix.is_program_id(MagicEdenV1.PROGRAM):
-            continue
-        logger.debug(f"Program is {MagicEdenV1.NAME}")
-        marketplace = MagicEdenV1.MARKETPLACE
-
-        if ix.data[0:10] == MagicEdenV1.LISTING_INSTRUCTION:
-            logger.debug("Is Listing instruction")
-
-            old_authority = ix["accounts"][0]  # 1st account
-            old_token_account = ix["accounts"][1]
-            listing_price = get_me_lamports_price_from_data(
-                ix.data, MagicEdenV1.PROGRAM
-            )
-            new_authority = MagicEdenV1.AUTHORITY
-
-            return ListingActivity(
-                transaction_id=tx.transaction_id,
-                block_time=tx.block_time,
-                slot=tx.slot,
-                mint=mint,
-                old_authority=old_authority,
-                new_authority=new_authority,
-                old_token_account=old_token_account,
-                new_token_account=old_token_account,  # In MEv1 token account doesn't change
-                price_lamports=listing_price,
-                program=marketplace,
-            )
+        activity = parse_ix(ix=ix)
+        if activity:
+            return activity
     return None
 
 
 def parse_listing_mev2(tx: Transaction, mint: str) -> Union[ListingActivity, None]:
+    def parse_ix(ix: Instruction) -> Union[ListingActivity, None]:
+        marketplace = marketplaces.magic_eden_v2
+
+        if not ix.is_program_id(marketplace.program):
+            return None
+        logger.debug(f"Program is {marketplace.name}")
+
+        if not ix.data[0:10] == marketplace.instructions.listing.base64_prefix:
+            return None
+        logger.debug("Is Listing instruction")
+
+        account_indices = marketplace.instructions.listing.account_indices
+
+        mint = ix["accounts"][account_indices.mint]
+        old_authority = ix["accounts"][account_indices.old_authority]
+        new_authority = marketplace.authority
+        old_token_account = ix["accounts"][account_indices.old_token_account]
+
+        price_lamports = get_me_lamports_price_from_data(ix.data, marketplace.program)
+
+        return ListingActivity(
+            transaction_id=tx.transaction_id,
+            block_time=tx.block_time,
+            slot=tx.slot,
+            mint=mint,
+            old_authority=old_authority,
+            new_authority=new_authority,
+            old_token_account=old_token_account,
+            new_token_account=old_token_account,  # In MEv2 token account doesn't change
+            price_lamports=price_lamports,
+            program=marketplace.name,
+        )
+
     for ix in tx.instructions.outer:
-        if not ix.is_program_id(MagicEdenV2.PROGRAM):
-            continue
-        logger.debug(f"Program is {MagicEdenV2.NAME}")
-        marketplace = MagicEdenV2.MARKETPLACE
-
-        if ix.data[0:10] == MagicEdenV2.LISTING_INSTRUCTION:
-            logger.debug("Is Listing instruction")
-            if ix["accounts"][4] != mint:  # 5th account
-                logger.debug("Mint did not match")
-
-            old_authority = ix["accounts"][0]  # 1st account
-            old_token_account = ix["accounts"][2]
-            listing_price = get_me_lamports_price_from_data(
-                ix.data, MagicEdenV2.PROGRAM
-            )
-            new_authority = MagicEdenV2.AUTHORITY
-
-            return ListingActivity(
-                transaction_id=tx.transaction_id,
-                block_time=tx.block_time,
-                slot=tx.slot,
-                mint=mint,
-                old_authority=old_authority,
-                new_authority=new_authority,
-                old_token_account=old_token_account,
-                new_token_account=old_token_account,  # In MEv2 token account doesn't change
-                price_lamports=listing_price,
-                program=marketplace,
-            )
+        activity = parse_ix(ix=ix)
+        if activity:
+            return activity
     return None
 
 
